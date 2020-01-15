88c739126bbf3951dbc9971912948f4698ac8c6e
Kajo1737 <kajo1737@student.ju.se>
2020-01-15 15:56:08 +0100

    added ToDo nad toDoRepository class

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -8,5 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+    val listView = theActivity.findViewById<ListView>(R.id.list_view)
+    listView.adapter = ArrayAdapter<ToDo>(
+        aContext,
+        android.R.layout.simple_list_item_1,
+        android.R.id.text1,
+        toDoRepository.getAllToDos()
+    )
 }
---
+++ app/src/main/java/com/example/todo/ToDo.kt
@@ -0,0 +1,12 @@
+package com.example.todo
+
+data class ToDo (
+    val id: Int,
+    var title: String,
+    var content: String
+){
+
+    override fun toString() = title
+
+
+}
